5931d46561bcb628234b0cf1832b3a772d088e8b
nissrinekache <80925035+nissrinekache@users.noreply.github.com>
2022-05-08 19:25:15 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$server ="localhost";
+$user="root";
+$pass="";
+$database ="library";
+
+$conn = mysqli_connect($server,$user,$pass,$database);
+
+if (!$conn) {
+    die( "<script> alert('Connection failed.') </script>");
+
+}
+?>
